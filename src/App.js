880aae7c6f67278444a831a30ef864955ed454b7
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-08-19 18:30:05 +0100

    Imported HeroComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route path="/" exact />
       </Routes>
