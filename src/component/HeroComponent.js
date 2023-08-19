45faef75ff25f549b919bda68a13475d10540169
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-08-19 18:21:30 +0100

    Created Hero Component and css

---
+++ src/component/HeroComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../HeroComponent.css";
+const HeroComponent = () => {
+  return (
+    <div className="hero-container">
+      <h1>
+        EXPLORE YOUR DREAM DESTINATIONS
+        <br /> WITH FLI
+      </h1>
+    </div>
+  );
+};
+
+export default HeroComponent;
