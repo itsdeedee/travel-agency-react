efc7941b47d65d6987db32bb64d5426772d83aba
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-08-21 21:35:34 +0100

    created Services component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
+import Services from "./component/Services";
 import "./App.css";
 
 function App() {
   return (
-    <div className="body">
+    <>
       <Navbar />
       <Routes>
         <Route path="/" exact />
       </Routes>
-    </div>
+      <Services />
+    </>
   );
 }
 
---
+++ src/component/Services.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../Services.css";
+
+function Services() {
+  return (
+    <div className="table">
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Services;
