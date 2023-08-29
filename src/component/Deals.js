db0c24b08cad80578f8d6c48de86e1bf13cd12c2
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-08-29 23:47:21 +0100

    Update Deals.js

---
+++ src/component/Deals.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../Deals.css";
+const Deals = () => {
+  return (
+    <div>
+      <h5>Best Deals & offers</h5>
+      <div className="Deals-1">
+        <div className="Deals-list1"></div>
+        <div className="Deals-list1">Family Trip Discount</div>
+      </div>
+      <div className="Deals-2">
+        <div className="Deals-list2">Couple's Date Trip</div>
+        <div className="Deals-list2"></div>
+      </div>
+      <div className="Deals-3">
+        <div className="Deals-list3"></div>
+        <div className="Deals-list3">Friend group Offers </div>
+      </div>
+    </div>
+  );
+};
+
+export default Deals;
