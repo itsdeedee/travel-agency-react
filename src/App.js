289e386ba5f5b85a69a7fc9d23a97596f80b4dc1
itsdeedee <123872266+itsdeedee@users.noreply.github.com>
2023-09-06 20:43:20 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import Navbar from "./component/Navbar";
 import Services from "./component/Services";
 import Banner from "./component/Banner";
 import Deals from "./component/Deals";
+import ProductImagesSlider from "./component/Gallery";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import Testimonials from "./component/Testimonials";
 
 import "./App.css";
+
+const productImages = [
+  require("./images/img2.jpg"),
+  require("./images/img3.jpg"),
+  require("./images/img4.jpg"),
+  require("./images/img5.jpg"),
+  require("./images/img6.jpg"),
+  require("./images/img7.jpg"),
+  require("./images/img8.jpg"),
+  require("./images/img9.jpg"),
+  require("./images/img10.jpg"),
+];
 
 function App() {
   return (
@@ -16,6 +33,8 @@ function App() {
       <Services />
       <Banner />
       <Deals />
+      <ProductImagesSlider images={productImages} />
+      <Testimonials />
     </>
   );
 }
